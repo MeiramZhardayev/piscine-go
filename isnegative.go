53d8b5c1d26590653b0dc84fb787b4b4c2cca4fb
MeiramZhardayev <meiram.zhardayev>
2019-10-16 18:36:25 +0600

    Isneg

--- isnegative.go
+++ isnegative.go
@@ -4,8 +4,8 @@ import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
 	if nb < 0 {
-		z01.PrintRune(rune('T'))
+		z01.PrintRune('T')
 	} else {
-		z01.PrintRune(rune('F'))
+		z01.PrintRune('F')
 	}
 }
