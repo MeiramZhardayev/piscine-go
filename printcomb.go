bd3150b026e2ac5d938b58e03fb0dfcb0bc115ba
MeiramZhardayev <meiram.zhardayev>
2019-10-16 20:04:07 +0600

    Hundreds

--- printcomb.go
+++ printcomb.go
@@ -5,25 +5,25 @@ import "github.com/01-edu/z01"
 func printComp() {
 	var a rune = 48
 	for i := a; i < 58; i++ {
-			for j := a; j < 58; j++ {
-					for k := a; k < 58; k++ {
-							if i < j {
-									if j < k {
-											if k != i {
-													z01.PrintRune(i)
-													z01.PrintRune(j)
-													z01.PrintRune(k)
-													if i == 55 {
+		for j := a; j < 58; j++ {
+			for k := a; k < 58; k++ {
+				if i < j {
+					if j < k {
+						if k != i {
+							z01.PrintRune(i)
+							z01.PrintRune(j)
+							z01.PrintRune(k)
+							if i == 55 {
 
-													} else {
-															z01.PrintRune(44)
-															z01.PrintRune(32)
-													}
-											}
-									}
+							} else {
+								z01.PrintRune(44)
+								z01.PrintRune(32)
 							}
+						}
 					}
+				}
 			}
+		}
 	}
 	z01.PrintRune(10)
 }
