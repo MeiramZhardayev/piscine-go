ad6668f98ff509b68e006879b4516f2cc57d4e62
MeiramZhardayev <meiram.zhardayev>
2019-10-16 19:30:23 +0600

    Hundreds

---
+++ printcomb.go
@@ -0,0 +1,28 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func printComp() {
+	var a rune = 48
+	for i := a, i < 58, i++ {
+			for j := a, j < 58, j++ {
+					for k := a, k < 58, k++ {
+							if i < j {
+									if j < k {
+											if z != i {
+													z01.PrintRune(i)
+													z01.PrintRune(j)
+													z01.PrintRune(k)
+													if i == 55 {
+
+													} else {
+															z01.PrintRune(44)
+															z01.PrintRune(32)
+											}
+									}
+							}
+					}
+			}
+	}
+	z01.PrintRune(10)
+}
