602eb8dd8780fcb43ef744d6a34c9b2a45cfe5b6
MeiramZhardayev <meiram.zhardayev>
2019-10-16 17:32:34 +0600

    Revalphabet

---
+++ printreversealphabet/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+
+	z01.PrintRune(rune(122))
+	z01.PrintRune(rune(121))
+	z01.PrintRune(rune(120))
+	z01.PrintRune(rune(119))
+	z01.PrintRune(rune(118))
+	z01.PrintRune(rune(117))
+	z01.PrintRune(rune(116))
+	z01.PrintRune(rune(115))
+	z01.PrintRune(rune(114))
+	z01.PrintRune(rune(113))
+	z01.PrintRune(rune(112))
+	z01.PrintRune(rune(111))
+	z01.PrintRune(rune(110))
+	z01.PrintRune(rune(109))
+	z01.PrintRune(rune(108))
+	z01.PrintRune(rune(107))
+	z01.PrintRune(rune(106))
+	z01.PrintRune(rune(105))
+	z01.PrintRune(rune(104))
+	z01.PrintRune(rune(103))
+	z01.PrintRune(rune(102))
+	z01.PrintRune(rune(101))
+	z01.PrintRune(rune(100))
+	z01.PrintRune(rune(99))
+	z01.PrintRune(rune(98))
+	z01.PrintRune(rune(97))
+
+	z01.PrintRune('\n')
+}
